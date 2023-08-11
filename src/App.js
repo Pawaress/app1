0afb8d72745098541a8757045d87ab2c5fa70ac2
Pawaress <110074181+Pawaress@users.noreply.github.com>
2023-08-11 15:00:51 +0700

    Create function-component.js and class-components.js
    create function-component.js and class-components.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import './style.css'
-
+import { Header,Content,Footer } from './func-components';
+import Calendar from './class-component';
 function App() {
 
-    return ( < > < h2 > DeveloperThai.com < /h2 >
+    /*return ([<Header/>,<Content/>,<Footer/>] );*/
+    return (
+    <>
+    <Header/>
+    <p><center><Calendar/></center></p>
+     <Content/> 
+     <Footer/>
+     </> 
+    
+    
+    
+    );
 
-        <
-        div className = 'title' > React & React Native < /div >
-
-        <
-        / > );
     }
 
 
