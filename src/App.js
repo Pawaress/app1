452a79d0b5991b72b817478fce73744e172c707c
Pawaress <110074181+Pawaress@users.noreply.github.com>
2023-09-22 15:31:17 +0700

    add all
    add all

--- src/App.js
+++ src/App.js
@@ -4,15 +4,13 @@ import Header from './context-header'
 import Content from './context-content'
 import Header2 from './context-header2'
 import Content2 from './context-content2'
+import Router2 from './router2'
+import Router3 from './router3'
 export default function App() {
-      let[user,setUser]=React.useState('')
+      
     return (
          
-        <userContext.Provider value={[user,setUser]}>
-        <Header2/>
-        <Content2/>
-       
-    </userContext.Provider>
+      <Router3/>
     )
     
     }
---
+++ src/products.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './products.css'
+export default class Products extends React.Component {
+    data=[
+        ['React',500],
+['React Native',650],
+['node.js',450],
+['MongoDB',300],
+['Express.js',650]]
+  render() {
+    return (
+      <table border = "1">
+        <tr><th>ชื่อ</th><th>ราคา</th></tr>
+        {
+            this.data.map(d=>{
+                return <tr><td>{d[0]}</td><td>{d[1]}</td></tr>
+            })
+        }
+      </table>
+    )
+  }
+}
