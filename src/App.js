964f5084febe9de8e7f8125cf2c29e606243a614
Pawaress <110074181+Pawaress@users.noreply.github.com>
2023-09-29 14:42:44 +0700

    add form-text
    add form-text

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ import Header2 from './context-header2'
 import Content2 from './context-content2'
 import Router2 from './router2'
 import Router3 from './router3'
+import FormText from './form-text'
 export default function App() {
       
     return (
          
-      <Router3/>
+      <FormText/>
     )
     
     }
